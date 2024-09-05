9bf49012611c6cef7eb193afa96ea3fbe9a713aa
Noorhesham <noordragon2004@gmail.com>
2024-09-05 22:24:52 +0200

    meow

--- app/[locale]/(home)/course/[id]/page.tsx
+++ app/[locale]/(home)/course/[id]/page.tsx
@@ -1,14 +1,11 @@
-import Course from "@/app/models/Course";
-import connect from "@/lib/clientPromise";
-import React from "react";
-export const generateStaticParams = async () => {
-  await connect();
-  const courses = await Course.find();
-  return courses.map((course: any) => course.id);
-};
-const page = async ({ params }: { params: { locale: string; id: string } }) => {
-  const course = await Course.findById(params.id);
-  return <section></section>;
-};
+import React from 'react'
 
-export default page;
+const page = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page

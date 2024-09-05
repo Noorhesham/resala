cc2cfae7fbdd1946ad052ce006e3095cee79757d
Noorhesham <noordragon2004@gmail.com>
2024-09-05 22:21:01 +0200

    meow

--- app/[locale]/(home)/course/[id]/page.tsx
+++ app/[locale]/(home)/course/[id]/page.tsx
@@ -1,6 +1,8 @@
 import Course from "@/app/models/Course";
+import connect from "@/lib/clientPromise";
 import React from "react";
 export const generateStaticParams = async () => {
+  await connect();
   const courses = await Course.find();
   return courses.map((course: any) => course.id);
 };
